0146907aa7d85f9f1d094fe847908c215749615e
ze37 <josealves6154@gmail.com>
2022-08-08 07:07:09 -0300

    Exercicio 2

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let num1 = 2;
-let num2 = 4;
+// EXERCÍCIO 2 
+// Elaborar um programa que leia o valor de um jantar. Calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+// EXEMPLO: 
+// Valor do jantar: R$80,00
+// Taxa do garçom: R$8,00
+// Total a pagar: R$88,00
+// ---------------------------------------------------------------
 
-console.log(num1 + num2);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
+let valorJantar = 80
+let taxaGarçom = 10
+let total = 0;
+
+total = 80 +((taxaGarçom / 100) * 80);
+
+console.log(`Valor do jantar: R$${valorJantar},00`)
+console.log(`Taxa do Garçom: R$${((taxaGarçom / 100) * 80)},00`);
+console.log(`Total a pagar: R$${total},00`);
