ea9fe77a34aebb71b2c039add22a8641678b737a
ze37 <josealvess6154@gmail.com>
2022-10-16 01:02:02 -0300

    estudo sobre condiçoes

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 18
+console.log(`A velocidade do carro percorrida ${vel} km/h`)
+if (vel > 80.5){
+    console.log('Voce ultrapasou o limite de velocidad, MULTADO')
+}
+console.log('Dirija sempre usando o sinto de sigurança')
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Lima'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brazil'){
+    console.log('Voce e brazileiro ')
+}else{
+    console.log("voce e estrangeiro")
+}
