9eff81b8d8c1e412e8340e7ebbb8bbe1180a3ace
ze37 <josealvess6154@gmail.com>
2022-11-05 06:25:28 -0300

    MUdança  no js

---
+++ Ex015/script.js
@@ -0,0 +1,47 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById("txtano")
+    var res = document.getElementById("res")
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10){
+                // crianca
+               img.setAttribute('src', 'CriaHome.png')
+            }else if (idade < 21){
+                // jovem
+                img.setAttribute('src','jovhome2.png')
+            }else if (idade < 50){
+                // Adulto
+                img.setAttribute('src','adulthome.png')
+            }else {
+                // Idoso
+                img.setAttribute('src','idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                // crianca
+            }else if (idade < 21){
+                // jovem
+            }else if (idade < 50){
+                // Adulto
+            }else {
+                // Idoso
+            }
+        }
+     
+        res.style.border = '10px'
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
+}
